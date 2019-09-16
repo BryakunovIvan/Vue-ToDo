14389c7085e7c5d3c1603bd6e70bfa67c6ce8092
Ivan_Briakunov <Ivan_Briakunov@epam.com>
2019-09-16 17:36:50 +0400

    no message

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="todo">
-    <div>{{title}}</div>
-    <div>{{description}}</div>
-    <div>{{date}}</div>
+    <div class="todo-head">
+      <h4 class="todo-title">{{title}}</h4>
+      <p class="todo-date">{{date}}</p>
+    </div>
+    <div class="todo-footer">
+      <p class="todo-description">{{description}}</p>
+    </div>
   </div>
 </template>
 
@@ -18,4 +22,32 @@ export default {
 </script>
 
 <style>
+.todo {
+  border: 1px solid rgba(128, 128, 128, 0.603);
+  border-radius: 10px;
+  width: 350px;
+  margin: 10px auto;
+  padding: 5px;
+}
+.todo-head {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.todo-title {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  margin: 0;
+}
+.todo-date {
+  margin: 0;
+}
+.todo-footer {
+  text-align: left;
+}
+.todo-description,
+.todo-title {
+  text-align: left;
+  width: 70%;
+}
 </style>
